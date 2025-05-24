685b37b92e4908b11f6e9ad02b36cdaf3150a378
Davidsonnj <davidsoncs45@gmail.com>
2025-05-24 18:35:25 -0300

    feat: ETL fato receita

---
+++ ETL_Fato_Receita.py
@@ -0,0 +1,89 @@
+import mysql.connector
+from datetime import datetime
+
+
+conn_relacional = mysql.connector.connect(
+    host='trabalho-kelly-banco-de-dados-2024-1.l.aivencloud.com',
+    port=25694,
+    user='avnadmin',
+    password='AVNS_qxYqWSPuEOEooRWhlXK',
+    database='dbnetf',
+)
+
+conn_dimensional = mysql.connector.connect(
+    host='modelo-dimensional-in-davidsonifes.d.aivencloud.com',
+    port=13844,
+    user='avnadmin',
+    password='AVNS_bDOXo9cSeE-GQIBLhRB',
+    database='dbnetf',
+)
+
+cursor_relacional = conn_relacional.cursor()
+cursor_dimensional = conn_dimensional.cursor()
+
+# Consulta no banco relacional
+cursor_relacional.execute('''
+SELECT 
+    p.vlrRecebido,
+    pl.nmPlano,
+    pl.precoMensal,
+    p.dtPagamento,
+    COUNT(a.idAssinatura) AS qtd_assinatura
+FROM Pagamento p
+JOIN Assinatura a ON p.Assinatura_idAssinatura = a.idAssinatura
+JOIN Plano pl ON a.Plano_idPlano = pl.idPlano
+GROUP BY p.vlrRecebido, pl.nmPlano, pl.precoMensal, p.dtPagamento;
+
+''')
+
+resultados = cursor_relacional.fetchall()
+
+for (vlRecebido, nmPlano, precoMensal, dtPagamento, qtd_assinatura) in resultados:
+
+     # --- Dim_Pagamento ---
+    cursor_dimensional.execute("SELECT idPagamento FROM Dim_Pagamento WHERE vlrRecebido = %s", (vlRecebido,))
+    row = cursor_dimensional.fetchone()
+    if row:
+        idPagamento = row[0]
+    else:
+        cursor_dimensional.execute("INSERT INTO Dim_Pagamento (vlrRecebido) VALUES (%s)", (vlRecebido,))
+        conn_dimensional.commit()
+        idPagamento = cursor_dimensional.lastrowid
+
+    # --- Dim_Plano ---
+    cursor_dimensional.execute("SELECT idPlano FROM Dim_Plano WHERE nmPlano = %s AND precoMensal = %s", (nmPlano, precoMensal))
+    row = cursor_dimensional.fetchone()
+    if row:
+        idPlano = row[0]
+    else:
+        cursor_dimensional.execute("INSERT INTO Dim_Plano (nmPlano, precoMensal) VALUES (%s, %s)", (nmPlano, precoMensal))
+        conn_dimensional.commit()
+        idPlano = cursor_dimensional.lastrowid
+    
+     # --- Dim_Data (Data de Pagamento) ---
+    data_pagamento = datetime.strptime(dtPagamento, '%Y-%m-%d')
+    mes = data_pagamento.month
+    ano = data_pagamento.year
+
+    cursor_dimensional.execute("SELECT idDim_Data FROM Dim_Data WHERE mes = %s AND ano = %s", (mes, ano))
+    row = cursor_dimensional.fetchone()
+    if row:
+        idData = row[0]
+    else:
+        cursor_dimensional.execute("INSERT INTO Dim_Data (mes, ano) VALUES (%s, %s)", (mes, ano))
+        conn_dimensional.commit()
+        idData = cursor_dimensional.lastrowid
+
+    # --- UPSERT na Fato_Receita ---
+    cursor_dimensional.execute("""
+        INSERT INTO Fato_Receita (
+            qtd_assinaturas,
+            fk_fato_receita_dim_plano,
+            fk_fato_receita_dim_pagamento,
+            fk_fato_receita_dim_data
+        ) VALUES (%s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            qtd_assinaturas = qtd_assinaturas + VALUES(qtd_assinaturas)
+    """, (qtd_assinatura, idPlano, idPagamento, idData))
+
+    conn_dimensional.commit()
